e3ecac25a925d4c744366083f02eee2daff0fe51
Preethika M <preethika.m@npcompete.com>
2022-08-11 07:15:04 +0530

    Wood Task Completetion

--- src/components/feature/errorScreen/molecules/content/index.js
+++ src/components/feature/errorScreen/molecules/content/index.js
@@ -1,9 +1,11 @@
 import Text from "../../atom/Text";
 import Button from "../../../../core/atom/Button";
+import Text1 from "../../../../core/atom/Text";
 import "../../../../core/style.css";
 import "../../molecules/style.css";
 export default function HeroSectionContent(){
     return(<>
+     <Text1 text="WOOPS" textDesigin="mb-4 new-font"></Text1>
     <Text text="Oh, you must be lost,there is no such page." textDesigin="mb-5 bold-30-fam2"></Text>
     <Button btnDesign="bold-30-fam2 homebtn" text="Go to the home page"></Button>
     </>);
